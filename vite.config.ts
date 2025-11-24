cdbd4e4443793e43840da287249dbb66547fb547
mhlas7 <3398654+mhlas7@users.noreply.github.com>
2025-11-23 18:04:18 -0800

    Very drafty offline mode

--- vite.config.ts
+++ vite.config.ts
@@ -2,11 +2,12 @@ import { o7Icon } from '@o7/icon/vite';
 import { sveltekit } from '@sveltejs/kit/vite';
 import tailwindcss from '@tailwindcss/vite';
 import { defineConfig } from 'vite';
-import { VitePWA, type VitePWAOptions } from 'vite-plugin-pwa';
+import { SvelteKitPWA, type VitePWAOptions } from '@vite-pwa/sveltekit';
 
 const pwaOptions: VitePWAOptions = {
-  injectRegister: 'auto',
   registerType: 'autoUpdate',
+  injectRegister: 'script',
+  strategies: 'generateSW',
   includeAssets: [
     'favicon.png',
     'favicon.svg',
@@ -39,11 +40,51 @@ const pwaOptions: VitePWAOptions = {
       },
     ],
   },
+  workbox: {
+    globPatterns: ['**/*.{js,css,png,svg,geojson}'],
+    sourcemap: true,
+    cleanupOutdatedCaches: true,
+    maximumFileSizeToCacheInBytes: 5 * 1024 * 1024, // 5 MB
+    runtimeCaching: [
+      /**
+       * Static assets cache: images and geojson file
+       */
+      {
+        urlPattern: /\.(?:png|svg|geojson)$/i,
+        handler: 'CacheFirst',
+        options: {
+          cacheName: 'airtrail-static',
+          expiration: {
+            maxEntries: 30,
+            maxAgeSeconds: 365 * 24 * 60 * 60, // 1 year
+          },
+        },
+      },
+      /**
+       * API data cache: flight data
+       */
+      {
+        urlPattern: /\/flight\/.*$/i,
+        method: 'GET',
+        handler: 'StaleWhileRevalidate',
+        options: {
+          cacheName: 'airtrail-userdata',
+          expiration: {
+            maxEntries: 30,
+            maxAgeSeconds: 7 * 24 * 60 * 60, // 7 days
+          },
+        },
+      },
+    ],
+  },
   devOptions: {
     enabled: true,
   },
+  minify: false,
+  includeManifestIcons: false,
+  disable: false
 };
 
 export default defineConfig({
-  plugins: [o7Icon(), tailwindcss(), sveltekit(), VitePWA(pwaOptions)],
+  plugins: [o7Icon(), tailwindcss(), sveltekit(), SvelteKitPWA(pwaOptions)],
 });
